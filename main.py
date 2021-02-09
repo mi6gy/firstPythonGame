f03b88ef6695f3dca96e04aad42177ba12258758
Miguel Jimenez <mjimenez08@gmail.com>
2021-02-08 20:02:45 -0500

    added name and age inputs

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Welcome to my first python game.")
+
+# user inputs
+name = input("What is your name? ")
+print("Hello " + name)
+age = input("What is your age? ")
+print("Hello", name, "you are", age, "years old")
+
+#check if player is old enoguh to play using conditions
+
+
+
+'''
+DATA TYPES
+str = anthing inside single or double quotation marks. a collection of characters or text. used to print
+int = any whole number. 1,4,7,235,-13
+float = any number with floating decimal point 4.5,2.0, -100.01
+bool = true or false
+'''
